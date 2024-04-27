a5aa5e8d329e5a53ea34ff4f4f432d1c3bd96116
Li-Kong <165821220+Li-Kong@users.noreply.github.com>
2024-04-27 23:45:58 +0200

    Add files via upload

--- src/components/Footer-Components/FooterComponent.vue
+++ src/components/Footer-Components/FooterComponent.vue
@@ -1,1 +1,40 @@
-
+<template>
+    <section id="footer">
+        <div class="container bg-light">
+            <div class="row">
+                <div class="col-md-6 d-flex justify-content-center align-items-center">
+                    <div class="social-footer">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-google-plus-g"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                    </div>
+                </div>
+                <div class="p-footer col-md-6 d-flex justify-content-center align-items-center">
+                    <p>© 2024 MegaOne. Made With Love By <a href="">Themesindustry</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+  </template>
+  
+  <script>
+  
+  </script>
+  
+  
+  <style scoped>
+  #footer{
+    background-color: #f8f9fa;
+    padding: 80px;
+    justify-content: center;
+    align-items: center;
+}
+.social-footer a {
+    color: #696969;
+    margin-right: 30px;
+}
+  </style>
+  
