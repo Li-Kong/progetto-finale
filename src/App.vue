d0e52f4b557e077ad85b2cd0ee7a7fb979a35816
Li-Kong <165821220+Li-Kong@users.noreply.github.com>
2024-04-27 23:49:01 +0200

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <NavbarComponent />
+  <JumbotronComponent />
+  <AboutusComponent />
+  <ServicesComponent />
+  <CountersComponent />
+  <TeamComponent />
+  <CentralComponent />
+  <PortfolioComponent />
+  <ClientsComponent />
+  <BlogComponent />
+  <SponsorComponent />
+  <MapComponent />
+  <ContactComponent />
+  <FooterComponent />
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavbarComponent from './components/Header-Components/NavbarComponent.vue'
+import JumbotronComponent from './components/Header-Components/JumbotronComponent.vue'
+import AboutusComponent from './components/Main-Components/AboutusComponent.vue'
+import ServicesComponent from './components/Main-Components/ServicesComponent.vue'
+import CountersComponent from './components/Main-Components/CountersComponent.vue'
+import TeamComponent from './components/Main-Components/TeamComponent.vue'
+import CentralComponent from './components/Main-Components/CentralComponent.vue'
+import PortfolioComponent from './components/Main-Components/PortfolioComponent.vue'
+import ClientsComponent from './components/Main-Components/ClientsComponent.vue'
+import BlogComponent from './components/Main-Components/BlogComponent.vue'
+import SponsorComponent from './components/Main-Components/SponsorComponent.vue'
+import MapComponent from './components/Main-Components/MapComponent.vue'
+import ContactComponent from './components/Main-Components/ContactComponent.vue'
+import FooterComponent from './components/Footer-Components/FooterComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavbarComponent,
+    JumbotronComponent,
+    AboutusComponent,
+    ServicesComponent,
+    CountersComponent,
+    TeamComponent,
+    CentralComponent,
+    PortfolioComponent,
+    ClientsComponent,
+    BlogComponent,
+    SponsorComponent,
+    MapComponent,
+    ContactComponent,
+    FooterComponent,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 </style>
